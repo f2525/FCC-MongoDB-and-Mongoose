cfc942bf7f4f0414fc33d88c074d5c9303f15546
f2525 <62139970+f2525@users.noreply.github.com>
2021-03-24 00:49:50 +0700

    updated

--- myApp.js
+++ myApp.js
@@ -72,7 +72,14 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id:personId}, function(err, found){
+    if(err){
+      console.log(err);
+    }
+    else{
+      done(null, found);
+    }
+  })
 };
 
 const findEditThenSave = (personId, done) => {
